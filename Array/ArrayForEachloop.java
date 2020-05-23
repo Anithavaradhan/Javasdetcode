9a8354c0bcf53e03c0a0b6bb6a396c57ea48273a
Anitha <anithaa357@gmail.com>
2020-05-24 00:15:15 +1200

    updated

---
+++ Array/ArrayForEachloop.java
@@ -0,0 +1,51 @@
+package Array;
+
+public class ArrayForEachloop {
+
+	public static void main(String[] args) {
+		
+		/*//int
+		
+		int a[] = new int[5];
+		a[0]=10;
+		a[1]=20;
+		a[2]=30;
+		a[3]=40;
+		a[4]=50;
+		
+		for(int i:a)
+		{
+			System.out.println(i);
+		}
+	}
+
+}*/
+
+
+// String
+
+String a[] = new String[5];
+a[0]="Anitha";
+a[1]="Welcome";
+a[2]="to";
+a[3]="Selenium";
+a[4]="java";
+
+    System.out.println(a.length);
+
+for(String i:a)
+{
+	//System.out.println(i);
+	//System.out.print(i);
+	
+	System.out.print(i + " ");
+}
+}
+
+} 
+
+
+/* 1) Declare an Array
+   2)Stroe elements in to Array
+   3)Read elements from an Array */
+/* Bala */
---
+++ Array/SingleDimensionalArray.java
@@ -0,0 +1,35 @@
+package Array;
+
+public class SingleDimensionalArray {
+
+	public static void main(String[] args) {
+		
+		int a[] = new int[5]; 
+		
+		/* declare array with5 elements
+		
+		 datatype store 1 variable = 1 value
+		 
+		 want to store 1 variable = multiple value */
+		 
+		
+		a[0]=10;
+		a[1]=20;
+		a[2]=30;
+		a[3]=40;
+		a[4]=50;
+		
+		for(int i=0;i<=4;i++)
+		//for(int i=0;i<a.lenght;i++)
+		{
+			System.out.println(a[i]);
+		}
+		
+			
+
+	}
+
+}
+
+
+
